e64200e098572a7f31dc49850f60fdf3016434e1
Victoria <126434697+vicpsantana@users.noreply.github.com>
2024-10-04 09:00:30 -0300

    Update UsuarioScreen.js

--- screens/UsuarioScreen.js
+++ screens/UsuarioScreen.js
@@ -1,19 +1,19 @@
 ////Victoria poiano Soares Sant`Ana
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import { Text, TextInput, Button, Appbar } from 'react-native-paper';
 
 export default function UsuarioScreen({ navigation }) {
   const [nome, setNome] = useState('');
   const [senha, setSenha] = useState('');
 
+  
+  const { width } = Dimensions.get('window');
+
   const handleLogin = () => {
-    // Aqui você pode adicionar lógica para autenticação
     if (nome && senha) {
-      // Se nome e senha estiverem preenchidos, navega para a tela Cliente
       navigation.navigate('Cliente');
     } else {
-      // Aqui você pode adicionar um alerta para informar o usuário que os campos são obrigatórios
       alert('Por favor, preencha todos os campos.');
     }
   };
@@ -23,19 +23,21 @@ export default function UsuarioScreen({ navigation }) {
       <Appbar.Header>
         <Appbar.Content title="Login de Usuário" />
       </Appbar.Header>
-      <View style={{ 
-          flex: 1, 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          padding: 20 
-      }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 20
+        }}
+      >
         <Text style={{ marginBottom: 20, fontSize: 20 }}>Bem-vindo!</Text>
 
         <TextInput
           label="Nome"
           value={nome}
           onChangeText={text => setNome(text)}
-          style={{ width: 300, marginBottom: 20 }} // Definindo uma largura fixa menor
+          style={{ width: width * 0.3, marginBottom: 15 }} // 30% da largura da tela
           mode="outlined"
         />
 
@@ -44,14 +46,14 @@ export default function UsuarioScreen({ navigation }) {
           value={senha}
           onChangeText={text => setSenha(text)}
           secureTextEntry
-          style={{ width: 300, marginBottom: 20 }} // Definindo uma largura fixa menor
+          style={{ width: width * 0.3, marginBottom: 15 }} // 30% da largura da tela
           mode="outlined"
         />
 
         <Button
           mode="contained"
           onPress={handleLogin}
-          style={{ marginTop: 20, width: 300 }} // Definindo a largura do botão
+          style={{ marginTop: 15, width: width * 0.3 }} // 30% da largura da tela
         >
           Entrar
         </Button>
